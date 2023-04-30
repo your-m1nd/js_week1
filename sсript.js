c023f224a74983487f6ea8064dc2085a89b252b0
your-m1nd <vi.neurogirl@gmail.com>
2023-04-30 22:44:58 +0300

    add index, js and 1-6 exercices

---
+++ sсript.js
@@ -0,0 +1,41 @@
+
+let janitor="40000";
+let teacher="22000";
+console.log(40000+22000);
+
+
+let city="Владивосток";
+let probel=" ";
+let n="2000";
+console.log("Владивосток"+" "+"2000");
+
+let chat="128";
+let like="254";
+let read="137";
+let youtube="201";
+console.log(128+254+137+201);
+
+
+let n1="2";
+let n2="5";
+console.log(n1 ** n2);
+
+
+let str="Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
+console.log(str);
+
+
+const greetings = {a:"Привет",};
+greetings.a = "Привет, ";
+const name = "Кот";
+console.log("Привет, "+"Кот");
+
+
+
+
+let fahrenheit=451;
+console.log(`${fahrenheit}`);
+let celcius=(`${fahrenheit-32}`);
+let celcius=(`419`);
+console.log(celcius/1.8);
+console.log(`'${fahrenheit} градуса по Фаренгейту — это ${celcius} градуса по Цельсию.'`);
