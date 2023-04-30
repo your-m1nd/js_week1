0be9684785b34680f9a4c8ee3f2e8876556a75ab
your-m1nd <vi.neurogirl@gmail.com>
2023-04-30 23:06:06 +0300

    add changes to exercise 8

--- sсript.js
+++ sсript.js
@@ -1,13 +1,17 @@
+//Задание 1
 
 let janitor="40000";
 let teacher="22000";
 console.log(40000+22000);
 
+//Задание 2
 
 let city="Владивосток";
 let probel=" ";
 let n="2000";
 console.log("Владивосток"+" "+"2000");
+
+//Задание 3
 
 let chat="128";
 let like="254";
@@ -15,27 +19,31 @@ let read="137";
 let youtube="201";
 console.log(128+254+137+201);
 
+//Задание 4
 
 let n1="2";
 let n2="5";
 console.log(n1 ** n2);
 
+//Задание 5
 
 let str="Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
 console.log(str);
 
+//Задание 6
 
 const greetings = {a:"Привет",};
 greetings.a = "Привет, ";
 const name = "Кот";
 console.log("Привет, "+"Кот");
 
+//Задание 7
 
 
+//Задание 8
 
 let fahrenheit=451;
-console.log(`${fahrenheit}`);
-let celcius=(`${fahrenheit-32}`);
-let celcius=(`419`);
-console.log(celcius/1.8);
+let celcius=(`${(fahrenheit-32)/1.8}`);
 console.log(`'${fahrenheit} градуса по Фаренгейту — это ${celcius} градуса по Цельсию.'`);
+
+
